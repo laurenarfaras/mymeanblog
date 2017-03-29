91534945d523b644860c31bacece797dbff76cdd
Lauren Arfaras <larfaras96@gmail.com>
2017-03-28 20:15:55 -0400

    added the edit view that requires a postId for selecting the editable post. used the  service in order to get the postid for the url

--- public/js/controllers/post.controller.js
+++ public/js/controllers/post.controller.js
@@ -3,9 +3,9 @@
   angular.module("mymeanblog")
         .controller("PostController", PostController);
 
-  PostController.$inject = ["$scope", "PostService"];
+  PostController.$inject = ["$scope", "PostService", "$routeParams"];
 
-  function PostController($scope, PostService) {
+  function PostController($scope, PostService, $routeParams) {
     $scope.create = create;
     $scope.edit = edit;
     $scope.mockPost = {
@@ -15,6 +15,15 @@
       created: new Date(),
       updated: new Date()
     };
+
+    var id = $routeParams.postId;
+    PostService.getOne(id)
+              .then(function(){
+                console.log("success");
+              })
+              .catch(function(){
+                console.log("error");
+              })
 
     function edit(post){
       console.log("editing post");
