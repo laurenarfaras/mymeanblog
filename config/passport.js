978000ab3df19c8eb41b67268f387a05c316290b
Lauren Arfaras <larfaras96@gmail.com>
2017-03-30 18:30:48 -0400

    created a local strategy to authenticate users with passportjs

--- config/passport.js
+++ config/passport.js
@@ -3,4 +3,22 @@ const passport = require("passport"); // module not file
 const LocalStrategy = require("passport-local").Strategy;
 const User = require("../models/user.model.js");
 
-passport.use(new LocalStrategy(), function howWeAuth(username, password, done){});
+passport.use(new LocalStrategy({ usernameField: "email"}),
+        function howWeAuth(username, password, done){
+          User.findOne({ email: username }, function(err, user){
+            if (err) {
+              return done(err);
+            }
+            if (!user) {  // if there is not a user with the email
+              return done(null, false, {
+                msg: "user not found"
+              });
+            }
+            if (!user.validPassword(password)){
+              return done(null, false, {
+                msg: "authentication failed"
+              });
+            }
+            return done(null, user);
+          });
+        });
