cce661c0b87e2556acec41135bd4d90a49dcada7
Lauren Arfaras <larfaras96@gmail.com>
2017-03-29 20:21:39 -0400

    added the jsonwebtoken and implemented the setpassword, validpassword, and generatejwt functions on the user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,6 +1,9 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require("jsonwebtoken");
+const signature = process.env.SIGNATURE || require("../secrets").SIGNATURE;
 
-var userSchema = mongoose.Schema({
+const userSchema = mongoose.Schema({
   email:{
     type: String,
     required: true,
@@ -18,9 +21,25 @@ var userSchema = mongoose.Schema({
   }
 });
 
-userSchema.methods.setPassword = function(password){};
-userSchema.methods.validPassword = function(password){};
-userSchema.methods.generateJwt = function(){};
+userSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString("hex");
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64);
+};
+
+userSchema.methods.validPassword = function(password){
+  const hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64);
+  return this.hash === hash;  // if they are equal then true, if not then false
+};
+userSchema.methods.generateJwt = function(){
+  const expiration = new Date();
+  expiration.setDate(expiration.getDate()+7); // create a date 7 days in the future
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    name: this.name,
+    exp: parseInt()
+  }, signature);
+};
 
 var User = mongoose.model('User', userSchema);
 module.exports = User;
